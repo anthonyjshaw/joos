79cc8a5a49f7fd0228c56d1b8e0d97a69844d4ff
Anthony Shaw <anthonyshaw@sceaga.com>
2021-11-30 19:05:24 +0000

    Added styles

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+
+const App = () => {
+	return (
+		<>
+		<Navbar />
+		<div className="container">
+			<h1>Creative problems need creative solutions.</h1>
+			<p>At Joos, we strive to give you the best in digital marketing solutions.</p>
+		</div>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Navbar = () => {
+	return (
+		<div className='navbar'>
+				<div className='nav-logo'>
+					<p>joos</p>
+				</div>
+				<div className='nav-items'>
+					<div>
+						<p>Work</p>
+					</div>
+					<div>
+						<p>About</p>
+					</div>
+					<div>
+						<p>Get in touch</p>
+					</div>
+				</div>
+		</div>
+	)
+}
+
+export default Navbar;
